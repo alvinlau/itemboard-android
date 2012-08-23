62f508614680c93cbf7d15556dfefdb8a08bf7af
Alvin Lau <mail@alvinlau.me>
2012-08-23 03:40:57 -0700

    make get request to /thing on create

--- src/com/itemboard/MainActivity.java
+++ src/com/itemboard/MainActivity.java
@@ -1,8 +1,14 @@
 package com.itemboard;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.util.Log;
+
+import com.turbomanage.httpclient.AsyncCallback;
+import com.turbomanage.httpclient.ParameterMap;
+import com.turbomanage.httpclient.HttpResponse;
+import com.turbomanage.httpclient.android.AndroidHttpClient;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +16,25 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        AndroidHttpClient httpClient = new AndroidHttpClient("http://173.255.210.55:7000");
+        httpClient.setMaxRetries(5);
+        httpClient.addHeader("Accept", "application/json");
+        httpClient.addHeader("Content-Type", "application/json");
+        ParameterMap params = httpClient.newParams();
+        httpClient.get("/thing", params, new AsyncCallback() {
+        	@Override
+            public void onComplete(HttpResponse httpResponse) {
+                //System.out.println(httpResponse.getBodyAsString());
+        		if(httpResponse != null) {
+        			Log.i("itemboard", httpResponse.getBodyAsString());
+        		}
+            }
+            @Override
+            public void onError(Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
